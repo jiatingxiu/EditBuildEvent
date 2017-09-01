7f099041781d8ba680eafea622105626225d57c0
jiatingxiu <jiatingxiu@chison.com.cn>
2017-09-01 15:42:59 +0800

    修改Test工程

---
+++ Test/SerialPortFixer.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Test
+{
+    // IDisposable
+    public class SerialPortFixer : IDisposable
+    {
+        public static void Execute(string portName)
+        {
+            using (new SerialPortFixer(portName))
+            {
+            }
+        }
+        #region "IDisposable Members"
+
+        public void Dispose()
+        {
+            if ((m_Handle != null))
+            {
+                m_Handle.Close();
+                m_Handle = null;
+            }
+        }
+
+        #endregion
+
+        #region "Implementation"
+
+        private const int DcbFlagAbortOnError = 14;
+        private const int CommStateRetries = 10;
+
+        private SafeFileHandle m_Handle;
+
+        private SerialPortFixer(string portName)
+        {
+            const int dwFlagsAndAttributes = 0x40000000;
+            const int dwAccess = 0x00000000;
+
+            if ((portName == null) || !(portName.StartsWith("COM", StringComparison.OrdinalIgnoreCase)))
+                throw new ArgumentException("Invalid Serial Port", "portName");
+
+            SafeFileHandle hFile = CreateFile("\\\\.\\" + portName, dwAccess, 0, IntPtr.Zero, 3, dwFlagsAndAttributes, IntPtr.Zero);
+            if ((hFile.IsInvalid))
+                WinIoError();
+
+            try
+            {
+                int fileType = GetFileType(hFile);
+                if (((fileType != 2) & (fileType != 0)))
+                    throw new ArgumentException("Invalid Serial Port", "portName");
+                m_Handle = hFile;
+                InitializeDcb();
+            }
+            catch
+            {
+                hFile.Close();
+                m_Handle = null;
+                throw;
+            }
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int FormatMessage(int dwFlags, HandleRef lpSource, int dwMessageId, int dwLanguageId, StringBuilder lpBuffer, int nSize, IntPtr arguments);
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool GetCommState(SafeFileHandle hFile, ref Dcb lpDcb);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool SetCommState(SafeFileHandle hFile, ref Dcb lpDcb);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool ClearCommError(SafeFileHandle hFile, ref int lpErrors, ref Comstat lpStat);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern SafeFileHandle CreateFile(string lpFileName, int dwDesiredAccess, int dwShareMode, IntPtr securityAttrs, int dwCreationDisposition, int dwFlagsAndAttributes, IntPtr hTemplateFile);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern int GetFileType(SafeFileHandle hFile);
+
+
+        private void InitializeDcb()
+        {
+            Dcb Dcb = new Dcb();
+            GetCommStateNative(ref Dcb);
+            Dcb.Flags = DcbFlagAbortOnError;
+            SetCommStateNative(ref Dcb);
+        }
+
+        private string GetMessage(int errorCode)
+        {
+            StringBuilder lpBuffer = new StringBuilder(0x200);
+            if ((FormatMessage(0x3200, new HandleRef(null, IntPtr.Zero), errorCode, 0, lpBuffer, lpBuffer.Capacity, IntPtr.Zero) != 0))
+            {
+                return lpBuffer.ToString();
+            }
+            return "Unknown Error";
+        }
+
+        private void WinIoError()
+        {
+            int errorCode = Marshal.GetLastWin32Error();
+            throw new IOException(GetMessage(errorCode), errorCode);
+        }
+
+        private void GetCommStateNative(ref Dcb lpDcb)
+        {
+            int commErrors = 0;
+            Comstat Comstat = new Comstat();
+            for (int i = 0; i <= CommStateRetries - 1; i++)
+            {
+                if (!(ClearCommError(m_Handle, ref commErrors, ref Comstat)))
+                    WinIoError();
+                if ((GetCommState(m_Handle, ref lpDcb)))
+                    return;
+                if ((i == CommStateRetries - 1))
+                    WinIoError();
+            }
+        }
+
+        private void SetCommStateNative(ref Dcb lpDcb)
+        {
+            int commErrors = 0;
+            Comstat Comstat = new Comstat();
+            for (int i = 0; i <= CommStateRetries - 1; i++)
+            {
+                if (!(ClearCommError(m_Handle, ref commErrors, ref Comstat)))
+                    WinIoError();
+                if ((SetCommState(m_Handle, ref lpDcb)))
+                    return;
+                if ((i == CommStateRetries - 1))
+                    WinIoError();
+            }
+        }
+
+        #region "Nested type: COMSTAT"
+
+        private struct Comstat
+        {
+
+            public readonly uint Flags;
+            public readonly uint cbInQue;
+
+            public readonly uint cbOutQue;
+        }
+        #endregion
+
+        #region "Nested type: DCB"
+
+        private struct Dcb
+        {
+
+            public readonly uint DCBlength;
+            public readonly uint BaudRate;
+            public uint Flags;
+            public readonly ushort wReserved;
+            public readonly ushort XonLim;
+            public readonly ushort XoffLim;
+            public readonly byte ByteSize;
+            public readonly byte Parity;
+            public readonly byte StopBits;
+            public readonly byte XonChar;
+            public readonly byte XoffChar;
+            public readonly byte ErrorChar;
+            public readonly byte EofChar;
+            public readonly byte EvtChar;
+            public readonly ushort wReserved1;
+        }
+        #endregion
+
+        #endregion
+    }
+}
--- Test/Window1.xaml.cs
+++ Test/Window1.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,6 +23,133 @@ namespace Test
         public Window1()
         {
             InitializeComponent();
+
+            var vv = "as".GetHashCode();
+            return;
+            //TextBox
+            //text.Typography.ContextualLigatures = false;
+            string sss = ToErJin("192.255.255.0").Trim('0');
+            var ip = IPAddress.Parse("10.5.1.61");
+            var mask = IPAddress.Parse("248.0.0.0");
+            var getway = IPAddress.Parse("10.6.254.254");
+            var result = ip.IsInSameSubnet(getway, mask);
+            ValidationIPSubMaskGetWay("10.5.1.61", "255.255.0.0", "10.5.254.254");
+        }
+
+        public bool ValidationIPSubMaskGetWay(string ip, string mask, string getway)
+        {
+            byte[] byteIP = IPAddress.Parse(ip).GetAddressBytes();
+            byte[] byteMask = IPAddress.Parse(mask).GetAddressBytes();
+            byte[] byteGetway = IPAddress.Parse(getway).GetAddressBytes();
+            return false;
+        }
+
+        /// <summary>
+        /// 转换为二进制
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ToErJin(string ipAddress)
+        {
+            var returnIpStr = "";
+            byte[] numArray = IPAddress.Parse(ipAddress).GetAddressBytes();
+            if (numArray.Length == 4)
+            {
+                for (var i = 0; i < 4; i++)
+                {
+                    var curr_num = numArray[i];
+                    var number_Bin = Convert.ToString(numArray[i], 2);
+                    var iCount = 8 - number_Bin.Length;
+                    for (var j = 0; j < iCount; j++)
+                    {
+                        number_Bin = "0" + number_Bin;
+                    }
+                    returnIpStr += number_Bin;
+                }
+            }
+            return returnIpStr;
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            //System.Windows.Forms.SendKeys.Send("{PGDN}");
+            webBrowser1.Navigate("javascript:var s = function() { window.scrollBy(0,-100); }; s();");
+        }
+    }
+
+    public static class SubnetMask
+    {
+        public static readonly IPAddress ClassA = IPAddress.Parse("255.0.0.0");
+        public static readonly IPAddress ClassB = IPAddress.Parse("255.255.0.0");
+        public static readonly IPAddress ClassC = IPAddress.Parse("255.255.255.0");
+
+        public static IPAddress CreateByHostBitLength(int hostpartLength)
+        {
+            int hostPartLength = hostpartLength;
+            int netPartLength = 32 - hostPartLength;
+
+            if (netPartLength < 2)
+                throw new ArgumentException("Number of hosts is to large for IPv4");
+
+            Byte[] binaryMask = new byte[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (i * 8 + 8 <= netPartLength)
+                    binaryMask[i] = (byte)255;
+                else if (i * 8 > netPartLength)
+                    binaryMask[i] = (byte)0;
+                else
+                {
+                    int oneLength = netPartLength - i * 8;
+                    string binaryDigit =
+                        String.Empty.PadLeft(oneLength, '1').PadRight(8, '0');
+                    binaryMask[i] = Convert.ToByte(binaryDigit, 2);
+                }
+            }
+            return new IPAddress(binaryMask);
+        }
+
+        public static IPAddress CreateByNetBitLength(int netpartLength)
+        {
+            int hostPartLength = 32 - netpartLength;
+            return CreateByHostBitLength(hostPartLength);
+        }
+
+        public static IPAddress CreateByHostNumber(int numberOfHosts)
+        {
+            int maxNumber = numberOfHosts + 1;
+
+            string b = Convert.ToString(maxNumber, 2);
+
+            return CreateByHostBitLength(b.Length);
+        }
+    }
+
+    public static class IPAddressExtensions
+    {
+        public static IPAddress GetNetworkAddress(this IPAddress address, IPAddress subnetMask)
+        {
+            byte[] ipAdressBytes = address.GetAddressBytes();
+            byte[] subnetMaskBytes = subnetMask.GetAddressBytes();
+
+            if (ipAdressBytes.Length != subnetMaskBytes.Length)
+                throw new ArgumentException("Lengths of IP address and subnet mask do not match.");
+
+            byte[] broadcastAddress = new byte[ipAdressBytes.Length];
+            for (int i = 0; i < broadcastAddress.Length; i++)
+            {
+                broadcastAddress[i] = (byte)(ipAdressBytes[i] & (subnetMaskBytes[i]));
+            }
+            return new IPAddress(broadcastAddress);
+        }
+
+        public static bool IsInSameSubnet(this IPAddress address2, IPAddress address, IPAddress subnetMask)
+        {
+            IPAddress network1 = address.GetNetworkAddress(subnetMask);
+            IPAddress network2 = address2.GetNetworkAddress(subnetMask);
+
+            return network1.Equals(network2);
         }
     }
 }
